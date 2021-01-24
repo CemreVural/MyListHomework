c1266079b9513bc95a672bb46600da2697c63125
CEMRE VURAL <vuralgsli@gmail.com>
2021-01-24 13:02:25 +0200

    Add project files.

---
+++ MyListHomework/MyDictionaryList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyListHomework
+{
+    class MyDictionaryList<Titem,Tprice>
+    {
+        Titem[] keys;
+        Tprice[] values;
+        public MyDictionaryList()
+        {
+            keys = new Titem[0];
+            values = new Tprice[0];
+        }
+        public void Add(Titem key, Tprice value)
+        {
+            Titem[] tempKeys = keys;
+            Tprice[] tempValues = values;
+
+            keys = new Titem[keys.Length + 1];
+            values = new Tprice[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public int Length
+        {
+            get
+            {
+                return keys.Length;
+            }
+        }
+        public Titem[] Keys
+        {
+            get
+            {
+                return keys;
+            }
+        }
+        public Tprice[] Values
+        {
+            get
+            {
+                return values;
+            }
+        }
+
+    }
+}
---
+++ MyListHomework/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace MyListHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionaryList<double, string> products = new MyDictionaryList<double, string>();
+            MyDictionaryList<double, string> MyDictionaryList = new MyDictionaryList<double, string>();
+            MyDictionaryList.Add(1, "Hand Guns");
+            MyDictionaryList.Add(2, "Sub Machine Guns");
+            MyDictionaryList.Add(3, "Heavy Machine Guns");
+            MyDictionaryList.Add(4, "Light Machine Guns");
+            MyDictionaryList.Add(5, "Non Lethal Guns");
+            MyDictionaryList.Add(6, "Melee Weapons");
+
+            Console.WriteLine("Categories: ->>>>\n");
+            for (int i = 0; i < MyDictionaryList.Length; i++)
+            {
+                Console.WriteLine("Code: " + MyDictionaryList.Keys[i] + " Name: " + MyDictionaryList.Values[i]);
+            }
+        }
+    }
+}
